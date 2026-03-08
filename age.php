c78b04b031272f093a7b78ac190597a7d8d96613
meera-9655 <aswini123shiva@gmail.com>
2026-03-08 13:03:34 +0530

    Update age.php

--- age.php
+++ age.php
@@ -3,12 +3,10 @@ error_reporting(1);
 $day=0;
 $yr=0;
 $mon=0;
-
 if(isset($_POST['b1']))
 {
 $d1=$_POST['t1'];
 $d2=$_POST['t2'];
-
 $arr=explode("/",$d1);
 $brr=explode("/",$d2);
 if($arr[0] < $brr[0])
@@ -17,7 +15,6 @@ $arr[0] += 30;   // corrected
 $arr[1] -= 1;    // corrected
 }
 $day = $arr[0] - $brr[0];
-
 if($arr[1] < $brr[1])
 {
 $arr[1] += 12;   // corrected ($m1 removed)
@@ -34,23 +31,19 @@ $yr = $arr[2] - $brr[2];
 <font color="orange"><h2><b>Age Calculator</b></h2></font>
 </td>
 </tr>
-
 <tr>
 <td align="center"><b>Enter current date (DD/MM/YYYY):</b></td>
 <td align="center"><input type="text" name="t1" autofocus></td>
 </tr>
-
 <tr>
 <td align="center"><b>Enter You DOB (DD/MM/YYYY):</b></td>
 <td align="center"><input type="text" name="t2"></td>
 </tr>
-
 <tr>
 <td align="center" colspan="2">
 <input type="submit" name="b1" value="calculate">
 </td>
 </tr>
-
 <tr>
 <td align="center"><b>Your age is:</b></td>
 <td align="center">
@@ -64,3 +57,4 @@ echo '</font>';
 </table>
 
 </form>
+
