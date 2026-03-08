15388e02ba97c39db186304a8c561db41d6c3bd5
meera-9655 <aswini123shiva@gmail.com>
2026-03-08 13:06:07 +0530

    Update factorial.php

--- factorial.php
+++ factorial.php
@@ -4,24 +4,23 @@
 </head>
 <body>
 <form method="post">
-    <input type="number" name="number" id="number" required>
-    <input type="submit" name="submit" value="Submit">
+<input type="number" name="number" id="number" required>
+<input type="submit" name="submit" value="Submit">
 </form>
-
 <?php
 if(isset($_POST['submit'])){
-    $fact = 1;
-    $number = $_POST['number'];
-
-    if($number < 0){
-        echo "Factorial not defined for negative numbers!";
-    } else {
-        for($i = 1; $i <= $number; $i++){
-            $fact = $fact * $i;
-        }
-        echo "<br>Factorial of $number is: $fact";
-    }
+$fact = 1;
+$number = $_POST['number'];
+if($number < 0){
+echo "Factorial not defined for negative numbers!";
+} else {
+for($i = 1; $i <= $number; $i++){
+$fact = $fact * $i;
+}
+echo "<br>Factorial of $number is: $fact";
+}
 }
 ?>
 </body>
 </html>
+
